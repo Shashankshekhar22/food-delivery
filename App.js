51a3adc9e9c500edd3c88749c3415dde9a8848d3
Shashankshekhar22 <itsshashankshekhar@gmail.com>
2024-05-04 22:26:30 +0530

    useState Implementation

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const Heading = () => <h1>This is food App</h1>;
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<Heading />);
